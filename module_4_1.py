e70373b21a8db7f7a19b698d7f921464825e6971
Людмила <severianka1@yandex.ru>
2024-11-11 17:57:58 +0300

    lesson16

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as schoolboy
+from true_math import divide as scientist
+
+result1 = schoolboy(69, 3)
+result2 = schoolboy(3, 0)
+result3 = scientist(49, 7)
+result4 = scientist(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
